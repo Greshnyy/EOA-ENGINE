de3b3ce65849a2a2a5d52ef7fbe561a9b6580e3b
Greshnyy <ybiliavskyi@gmail.com>
2026-08-10 07:59:30 +0200

    Add configurable fixed timestep to TimeSystem

--- eoa-engine/src/core/Systems.h
+++ eoa-engine/src/core/Systems.h
@@ -27,7 +27,6 @@ struct Time {
 
     Seconds GetTimeScale() const { return timeScale_; }
     void SetTimeScale(Seconds scale) { timeScale_ = std::max(0.0, scale); }
-
     Seconds GetRealDeltaTime() const { return realDeltaTime_; }
     void SetRealDeltaTime(Seconds dt) { realDeltaTime_ = std::max(0.0, dt); }
 
@@ -39,6 +38,10 @@ private:
 class TimeSystem {
 public:
     TimeSystem() : lastFrameTime_(Clock::now()) {}
+
+    void SetFixedDeltaTime(Seconds dt) {
+        time_.fixedDeltaTime = std::max(0.0001, dt);
+    }
 
     void Tick() {
         const auto now = Clock::now();
@@ -55,22 +58,16 @@ public:
         ++time_.frameCount;
 
         frameTimes_.push_back(realDelta);
-        if (frameTimes_.size() > 60) {
-            frameTimes_.erase(frameTimes_.begin());
-        }
+        if (frameTimes_.size() > 60) frameTimes_.erase(frameTimes_.begin());
 
         Seconds avgTime = 0.0;
         for (const Seconds t : frameTimes_) avgTime += t;
-        if (avgTime > 0.0) {
-            time_.fps = static_cast<float>(frameTimes_.size() / avgTime);
-        }
+        if (avgTime > 0.0) time_.fps = static_cast<float>(frameTimes_.size() / avgTime);
 
         fixedTimeAccumulator_ += time_.deltaTime;
     }
 
-    bool ShouldFixedUpdate() const {
-        return fixedTimeAccumulator_ >= time_.fixedDeltaTime;
-    }
+    bool ShouldFixedUpdate() const { return fixedTimeAccumulator_ >= time_.fixedDeltaTime; }
 
     bool ConsumeFixedStep() {
         if (!ShouldFixedUpdate()) return false;
@@ -79,24 +76,19 @@ public:
     }
 
     void DoFixedUpdate() { ConsumeFixedStep(); }
-
     const Time& GetTime() const { return time_; }
 
     static Seconds Now() {
-        const auto now = Clock::now();
-        return std::chrono::duration<Seconds>(now.time_since_epoch()).count();
+        return std::chrono::duration<Seconds>(Clock::now().time_since_epoch()).count();
     }
 
     static void Sleep(Milliseconds ms) {
-        if (ms > 0.0) {
-            std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(ms));
-        }
+        if (ms > 0.0) std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(ms));
     }
 
 private:
     using Clock = std::chrono::steady_clock;
     using TimePoint = Clock::time_point;
-
     TimePoint lastFrameTime_;
     Seconds fixedTimeAccumulator_ = 0.0;
     Time time_;
@@ -108,17 +100,12 @@ using LogCallback = std::function<void(LogLevel, const std::string&)>;
 
 class Logger {
 public:
-    static Logger& GetInstance() {
-        static Logger instance;
-        return instance;
-    }
-
+    static Logger& GetInstance() { static Logger instance; return instance; }
     void SetLevel(LogLevel level) { minLevel_ = level; }
     void SetCallback(LogCallback callback) { callback_ = std::move(callback); }
 
     void Log(LogLevel level, const std::string& message) {
         if (level < minLevel_) return;
-
         std::string prefix;
         switch (level) {
             case LogLevel::Trace: prefix = "[TRACE]"; break;
@@ -128,12 +115,10 @@ public:
             case LogLevel::Error: prefix = "[ERROR]"; break;
             case LogLevel::Fatal: prefix = "[FATAL]"; break;
         }
-
         const std::string fullMessage = prefix + " " + message;
         if (callback_) callback_(level, fullMessage);
         else if (level >= LogLevel::Error) std::fprintf(stderr, "%s\n", fullMessage.c_str());
         else std::printf("%s\n", fullMessage.c_str());
-
         if (level == LogLevel::Fatal) std::abort();
     }
 
@@ -161,10 +146,7 @@ using Task = std::function<void()>;
 
 class TaskManager {
 public:
-    static TaskManager& GetInstance() {
-        static TaskManager instance;
-        return instance;
-    }
+    static TaskManager& GetInstance() { static TaskManager instance; return instance; }
 
     void AddTask(Task task) {
         if (!task) return;
@@ -190,12 +172,9 @@ public:
                 if (it->delay <= 0.0) {
                     readyDelayedTasks.push_back(std::move(it->task));
                     it = delayedTasks_.erase(it);
-                } else {
-                    ++it;
-                }
+                } else ++it;
             }
         }
-
         for (auto& task : localTasks) if (task) task();
         for (auto& task : readyDelayedTasks) if (task) task();
     }
@@ -223,12 +202,10 @@ public:
     virtual void Update(Seconds) {}
     virtual void FixedUpdate(Seconds) {}
     virtual void Render() {}
-
     int GetPriority() const { return priority_; }
     void SetPriority(int priority) { priority_ = priority; }
     bool IsEnabled() const { return enabled_; }
     void SetEnabled(bool enabled) { enabled_ = enabled; }
-
 protected:
     int priority_ = 0;
     bool enabled_ = true;
