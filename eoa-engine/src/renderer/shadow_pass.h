f16bc8154ec5208ab80eaeecbb62d88903a945e0
Greshnyy <ybiliavskyi@gmail.com>
2026-08-10 08:21:20 +0200

    Add ShadowPass class for directional shadow mapping

---
+++ eoa-engine/src/renderer/shadow_pass.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace eoa {
+
+class Pipeline;
+
+// Shadow mapping pass: renders depth from light's view for directional light
+class ShadowPass {
+public:
+    static constexpr uint32_t kShadowMapSize = 2048;
+
+    ShadowPass(VkPhysicalDevice physical, VkDevice device, uint32_t graphicsFamily);
+    ~ShadowPass();
+
+    ShadowPass(const ShadowPass&) = delete;
+    ShadowPass& operator=(const ShadowPass&) = delete;
+
+    void CreatePipeline(VkDescriptorSetLayout cameraSetLayout);
+    void Resize(uint32_t width, uint32_t height);
+
+    VkRenderPass GetRenderPass() const { return renderPass_; }
+    VkFramebuffer GetFramebuffer() const { return framebuffer_; }
+    VkPipeline GetPipeline() const { return pipeline_->Handle(); }
+    VkPipelineLayout GetLayout() const { return pipeline_->Layout(); }
+    VkImageView GetDepthImageView() const { return depthImageView_; }
+    VkSampler GetDepthSampler() const { return depthSampler_; }
+    VkExtent2D GetExtent() const { return extent_; }
+
+    // Light view-projection matrix
+    glm::mat4 GetLightViewProj() const { return lightViewProj_; }
+    void SetLightViewProj(const glm::mat4& vp) { lightViewProj_ = vp; }
+
+private:
+    VkPhysicalDevice physical_;
+    VkDevice device_;
+    uint32_t graphicsFamily_;
+
+    VkExtent2D extent_{kShadowMapSize, kShadowMapSize};
+    VkImage depthImage_ = VK_NULL_HANDLE;
+    VkDeviceMemory depthImageMemory_ = VK_NULL_HANDLE;
+    VkImageView depthImageView_ = VK_NULL_HANDLE;
+    VkSampler depthSampler_ = VK_NULL_HANDLE;
+
+    VkRenderPass renderPass_ = VK_NULL_HANDLE;
+    VkFramebuffer framebuffer_ = VK_NULL_HANDLE;
+    VkCommandPool commandPool_ = VK_NULL_HANDLE;
+
+    std::unique_ptr<Pipeline> pipeline_;
+    glm::mat4 lightViewProj_ = glm::mat4(1.0f);
+
+    void CreateImages();
+    void CreateRenderPass();
+    void CreateFramebuffer();
+    void CreateCommandPool();
+    void DestroyShadowResources();
+};
+
+} // namespace eoa
