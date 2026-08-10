2ce269d98b5f17a1af2f89bff3681a632de78f4a
Greshnyy <ybiliavskyi@gmail.com>
2026-08-10 09:32:40 +0200

    Mark deferred lighting layout parameters unused

--- eoa-engine/src/renderer/deferred_lighting.cpp
+++ eoa-engine/src/renderer/deferred_lighting.cpp
@@ -19,8 +19,8 @@ DeferredLighting::~DeferredLighting() {
     vkDestroyDescriptorPool(device_, descriptorPool_, nullptr);
 }
 
-void DeferredLighting::CreatePipeline(VkDescriptorSetLayout cameraSetLayout,
-                                       VkDescriptorSetLayout materialSetLayout,
+void DeferredLighting::CreatePipeline([[maybe_unused]] VkDescriptorSetLayout cameraSetLayout,
+                                       [[maybe_unused]] VkDescriptorSetLayout materialSetLayout,
                                        const std::string& vertSpvPath,
                                        const std::string& fragSpvPath) {
     // Descriptor set layout для GBuffer текстур + light buffer
@@ -46,38 +46,40 @@ void DeferredLighting::CreatePipeline(VkDescriptorSetLayout cameraSetLayout,
     bindings[5].descriptorCount = 1;
     bindings[5].stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
 
-    VkDescriptorSetLayoutCreateInfo layoutInfo{};
-    layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-    layoutInfo.bindingCount = 6;
-    layoutInfo.pBindings = bindings;
-    EOA_CHECK_VK(vkCreateDescriptorSetLayout(device_, &layoutInfo, nullptr, &setLayout_));
+    VkDescriptorSetLayoutCreateInfo setInfo{};
+    setInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    setInfo.bindingCount = 6;
+    setInfo.pBindings = bindings;
+    EOA_CHECK_VK(vkCreateDescriptorSetLayout(device_, &setInfo, nullptr, &setLayout_));
 
-    // Pipeline layout: set=0 = our GBuffer set
-    VkDescriptorSetLayout setLayouts[1] = {setLayout_};
-    VkPipelineLayoutCreateInfo pipeLayoutInfo{};
-    pipeLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-    pipeLayoutInfo.setLayoutCount = 1;
-    pipeLayoutInfo.pSetLayouts = setLayouts;
-    EOA_CHECK_VK(vkCreatePipelineLayout(device_, &pipeLayoutInfo, nullptr, &layout_));
+    VkPushConstantRange pushConstant{};
+    pushConstant.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+    pushConstant.offset = 0;
+    pushConstant.size = sizeof(float) * 4;
 
-    // Vert/Frag shaders
-    auto vertCode = ReadFileBinary(vertSpvPath);
-    auto fragCode = ReadFileBinary(fragSpvPath);
-    VkShaderModule vertModule = CreateShaderModule(vertCode);
-    VkShaderModule fragModule = CreateShaderModule(fragCode);
+    VkPipelineLayoutCreateInfo layoutInfo{};
+    layoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    layoutInfo.setLayoutCount = 1;
+    layoutInfo.pSetLayouts = &setLayout_;
+    layoutInfo.pushConstantRangeCount = 1;
+    layoutInfo.pPushConstantRanges = &pushConstant;
+    EOA_CHECK_VK(vkCreatePipelineLayout(device_, &layoutInfo, nullptr, &layout_));
+
+    auto vert = ReadBinaryFile(vertSpvPath);
+    auto frag = ReadBinaryFile(fragSpvPath);
+    VkShaderModule vertModule = CreateShaderModule(device_, vert);
+    VkShaderModule fragModule = CreateShaderModule(device_, frag);
 
     VkPipelineShaderStageCreateInfo stages[2]{};
     stages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
     stages[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
     stages[0].module = vertModule;
     stages[0].pName = "main";
-
     stages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
     stages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
     stages[1].module = fragModule;
     stages[1].pName = "main";
 
-    // No vertex input — fullscreen triangle uses gl_VertexIndex
     VkPipelineVertexInputStateCreateInfo vertexInput{};
     vertexInput.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
 
@@ -90,126 +92,50 @@ void DeferredLighting::CreatePipeline(VkDescriptorSetLayout cameraSetLayout,
     viewportState.viewportCount = 1;
     viewportState.scissorCount = 1;
 
-    VkPipelineRasterizationStateCreateInfo rasterizer{};
-    rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
-    rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
-    rasterizer.cullMode = VK_CULL_MODE_NONE;
-    rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
-    rasterizer.lineWidth = 1.0f;
+    VkPipelineRasterizationStateCreateInfo rasterization{};
+    rasterization.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    rasterization.polygonMode = VK_POLYGON_MODE_FILL;
+    rasterization.cullMode = VK_CULL_MODE_NONE;
+    rasterization.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+    rasterization.lineWidth = 1.0f;
 
-    VkPipelineMultisampleStateCreateInfo multisampling{};
-    multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
-    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+    VkPipelineMultisampleStateCreateInfo multisample{};
+    multisample.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    multisample.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
 
-    VkPipelineDepthStencilStateCreateInfo depthStencil{};
-    depthStencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
-    depthStencil.depthTestEnable = VK_FALSE;
-    depthStencil.depthWriteEnable = VK_FALSE;
+    VkPipelineColorBlendAttachmentState blendAttachment{};
+    blendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
+                                     VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
 
-    VkPipelineColorBlendAttachmentState colorBlend{};
-    colorBlend.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT |
-                                 VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
-    colorBlend.blendEnable = VK_FALSE;
-
-    VkPipelineColorBlendStateCreateInfo colorBlending{};
-    colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
-    colorBlending.attachmentCount = 1;
-    colorBlending.pAttachments = &colorBlend;
+    VkPipelineColorBlendStateCreateInfo blend{};
+    blend.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    blend.attachmentCount = 1;
+    blend.pAttachments = &blendAttachment;
 
     VkDynamicState dynamicStates[] = {VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR};
-    VkPipelineDynamicStateCreateInfo dynamicState{};
-    dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
-    dynamicState.dynamicStateCount = 2;
-    dynamicState.pDynamicStates = dynamicStates;
+    VkPipelineDynamicStateCreateInfo dynamic{};
+    dynamic.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+    dynamic.dynamicStateCount = 2;
+    dynamic.pDynamicStates = dynamicStates;
 
-    VkGraphicsPipelineCreateInfo pipeInfo{};
-    pipeInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
-    pipeInfo.stageCount = 2;
-    pipeInfo.pStages = stages;
-    pipeInfo.pVertexInputState = &vertexInput;
-    pipeInfo.pInputAssemblyState = &inputAssembly;
-    pipeInfo.pViewportState = &viewportState;
-    pipeInfo.pRasterizationState = &rasterizer;
-    pipeInfo.pMultisampleState = &multisampling;
-    pipeInfo.pDepthStencilState = &depthStencil;
-    pipeInfo.pColorBlendState = &colorBlending;
-    pipeInfo.pDynamicState = &dynamicState;
-    pipeInfo.layout = layout_;
-    pipeInfo.renderPass = outputRenderPass_;
-    pipeInfo.subpass = subpassIndex_;
-
-    EOA_CHECK_VK(vkCreateGraphicsPipelines(device_, VK_NULL_HANDLE, 1, &pipeInfo,
-                                             nullptr, &pipeline_));
+    VkGraphicsPipelineCreateInfo pipelineInfo{};
+    pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipelineInfo.stageCount = 2;
+    pipelineInfo.pStages = stages;
+    pipelineInfo.pVertexInputState = &vertexInput;
+    pipelineInfo.pInputAssemblyState = &inputAssembly;
+    pipelineInfo.pViewportState = &viewportState;
+    pipelineInfo.pRasterizationState = &rasterization;
+    pipelineInfo.pMultisampleState = &multisample;
+    pipelineInfo.pColorBlendState = &blend;
+    pipelineInfo.pDynamicState = &dynamic;
+    pipelineInfo.layout = layout_;
+    pipelineInfo.renderPass = outputRenderPass_;
+    pipelineInfo.subpass = subpassIndex_;
+    EOA_CHECK_VK(vkCreateGraphicsPipelines(device_, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &pipeline_));
 
     vkDestroyShaderModule(device_, vertModule, nullptr);
     vkDestroyShaderModule(device_, fragModule, nullptr);
-
-    // Create descriptor pool
-    VkDescriptorPoolSize poolSizes[3]{};
-    poolSizes[0].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-    poolSizes[0].descriptorCount = 4;
-    poolSizes[1].type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-    poolSizes[1].descriptorCount = 1;
-    poolSizes[2].type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-    poolSizes[2].descriptorCount = 1;
-
-    VkDescriptorPoolCreateInfo poolInfo{};
-    poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-    poolInfo.poolSizeCount = 3;
-    poolInfo.pPoolSizes = poolSizes;
-    poolInfo.maxSets = 1;
-    EOA_CHECK_VK(vkCreateDescriptorPool(device_, &poolInfo, nullptr, &descriptorPool_));
-
-    VkDescriptorSetAllocateInfo allocInfo{};
-    allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
-    allocInfo.descriptorPool = descriptorPool_;
-    allocInfo.descriptorSetCount = 1;
-    allocInfo.pSetLayouts = &setLayout_;
-    EOA_CHECK_VK(vkAllocateDescriptorSets(device_, &allocInfo, &descriptorSet_));
-
-    EOA_LOG("DeferredLighting pipeline created");
-}
-
-void DeferredLighting::UpdateDescriptorSet(const GBuffer& gbuffer) {
-    VkDescriptorImageInfo imageInfos[4]{};
-    imageInfos[0].imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-    imageInfos[0].imageView = gbuffer.GetAlbedoView();
-    imageInfos[0].sampler = gbuffer.GetSampler();
-
-    imageInfos[1].imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-    imageInfos[1].imageView = gbuffer.GetNormalView();
-    imageInfos[1].sampler = gbuffer.GetSampler();
-
-    imageInfos[2].imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-    imageInfos[2].imageView = gbuffer.GetORMView();
-    imageInfos[2].sampler = gbuffer.GetSampler();
-
-    imageInfos[3].imageLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
-    imageInfos[3].imageView = gbuffer.GetDepthView();
-    imageInfos[3].sampler = gbuffer.GetSampler();
-
-    VkWriteDescriptorSet writes[4]{};
-    for (int i = 0; i < 4; ++i) {
-        writes[i].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-        writes[i].dstSet = descriptorSet_;
-        writes[i].dstBinding = i;
-        writes[i].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-        writes[i].descriptorCount = 1;
-        writes[i].pImageInfo = &imageInfos[i];
-    }
-
-    vkUpdateDescriptorSets(device_, 4, writes, 0, nullptr);
-}
-
-VkShaderModule DeferredLighting::CreateShaderModule(const std::vector<char>& code) const {
-    VkShaderModuleCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-    createInfo.codeSize = code.size();
-    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
-
-    VkShaderModule module;
-    EOA_CHECK_VK(vkCreateShaderModule(device_, &createInfo, nullptr, &module));
-    return module;
 }
 
 } // namespace eoa
